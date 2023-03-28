bc5dcd1021f5e02a165a17c71c88761c9d5072db
Sergei Begletsov <snower87@yandex.ru>
2023-03-28 13:47:02 +0500

    JS. Урок №2 - Практика

---
+++ course_javascript/old_code_mu/lesson_02/task_02_char_str_02.js
@@ -0,0 +1,10 @@
+/*
+Создайте переменную num и присвойте ей значение '12345'. Найдите произведение (умножение) цифр этого числа.
+*/
+var num = '12345';
+var n1 = Number(num[0]);
+var n2 = Number(num[1]);
+var n3 = Number(num[2]);
+var n4 = Number(num[3]);
+var n5 = Number(num[4]);
+alert(n1 * n2 * n3 * n4 * n5);
